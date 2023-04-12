03086476b5927cb60f356c03e11c55fe5a8a83b9
Ajmal junaid <ajmal6201@gmail.com>
2023-04-12 12:25:13 +0530

    added origin in socket

--- utils/socket.js
+++ utils/socket.js
@@ -3,7 +3,7 @@ const { joinRoom, addMessage } = require('../controllers/community')
 function initializeSocket(server) {
   const io = new Server(server, {
     cors: {
-      origin: ['http://localhost:5173','http://localhost:3001', '*']
+      origin: ['https://www.eaglone.online','http://localhost:5173','http://localhost:3001', '*']
     }
   });
 
