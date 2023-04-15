0a669a1acb3042cee346529fe9d174fc165f8733
Ajmal junaid <ajmal6201@gmail.com>
2023-04-15 09:27:30 +0530

    mongo connection new engine

--- utils/database.js
+++ utils/database.js
@@ -1,3 +1,6 @@
 const mongoose = require('mongoose')
-const connection = mongoose.createConnection(process.env.MONGO_URI_LEARNING)
+const connection = mongoose.createConnection(process.env.MONGO_URI_LEARNING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 module.exports = connection
