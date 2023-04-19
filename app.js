b86f47dd7d203f1d6e416ba4adb00b06774f8d58
Ajmal junaid <ajmal6201@gmail.com>
2023-04-20 00:12:45 +0530

    log updated

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const PORT = 4000
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-app.use(morgan('dev'));
+app.use(morgan('combined'));
 
 initializeSocket(server);
 
